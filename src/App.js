371a6cc9e19562988eed625b38ecf1ea483c7198
Jarod Schoeman <theshoebruh@gmail.com>
2025-02-18 08:48:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: '#add8e6', textAlign: 'center', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <img src={"https://media.tenor.com/_MyfVFG3LtMAAAAM/cow-eatjng.gif"} className="App-logo" alt="logo" style={{ marginBottom: '20px' }} />
+      <p style={{ color: 'black', fontSize: 25, marginLeft: 20, marginRight: 20 }}>
+      Isaiah 41:10: "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you" 
+      </p>
     </div>
   );
 }
